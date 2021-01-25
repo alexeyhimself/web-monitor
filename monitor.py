683a4eb6a0e312d4a0647f23e6f6bc34c900a37b
Alexey Sotskov <ihimself@gmail.com>
2021-01-25 23:40:25 +0300

    call_server now in a separate file

--- monitor.py
+++ monitor.py
@@ -1,7 +1,5 @@
-import requests
 from libs.config_loader import load_config
-
-DEFAULT_TIMEOUT = 10  # seconds
+from libs.servers_caller import call_server
 
 
 if __name__ == '__main__':
@@ -10,23 +8,7 @@ if __name__ == '__main__':
   monitored_urls = cfg.get("monitored_urls", [])
   if monitored_urls:
     for each_url in monitored_urls:
-      url = each_url.get("url")
-      timeout = each_url.get("timeout", DEFAULT_TIMEOUT)
-
-      try:
-        r = requests.get(url, timeout=timeout)
-        msg = url + ': ' + str(r.status_code)
-        print(msg)
-
-      except requests.exceptions.ConnectionError:
-        msg = url + ': ' + "ConnectionError"
-        print(msg)
-      except requests.exceptions.Timeout:
-        msg = url + ': ' + "ConnectionError"
-        print(msg)
-      except Exception as why:
-        msg = url + ': ' + str(why)
-        print(msg)
+      call_server(each_url)
 
   else:
     print("URL(s) to monitor have not been provided in config")

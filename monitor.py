44debeac8a2f78daf4a4726eb2fe444086ec8dbb
Alexey Sotskov <ihimself@gmail.com>
2021-01-25 23:17:05 +0300

    introduced main function to monitor app core

--- monitor.py
+++ monitor.py
@@ -3,28 +3,30 @@ from libs.config_loader import load_config
 
 DEFAULT_TIMEOUT = 10  # seconds
 
-cfg = load_config()
 
-monitored_urls = cfg.get("monitored_urls", [])
-if monitored_urls:
-  for each_url in monitored_urls:
-    url = each_url.get("url")
-    timeout = each_url.get("timeout", DEFAULT_TIMEOUT)
+if __name__ == '__main__':
+  cfg = load_config()
 
-    try:
-      r = requests.get(url, timeout=timeout)
-      msg = url + ': ' + str(r.status_code)
-      print(msg)
+  monitored_urls = cfg.get("monitored_urls", [])
+  if monitored_urls:
+    for each_url in monitored_urls:
+      url = each_url.get("url")
+      timeout = each_url.get("timeout", DEFAULT_TIMEOUT)
 
-    except requests.exceptions.ConnectionError:
-      msg = url + ': ' + "ConnectionError"
-      print(msg)
-    except requests.exceptions.Timeout:
-      msg = url + ': ' + "ConnectionError"
-      print(msg)
-    except Exception as why:
-      msg = url + ': ' + str(why)
-      print(msg)
+      try:
+        r = requests.get(url, timeout=timeout)
+        msg = url + ': ' + str(r.status_code)
+        print(msg)
 
-else:
-  print("URL(s) to monitor have not been provided in config")
+      except requests.exceptions.ConnectionError:
+        msg = url + ': ' + "ConnectionError"
+        print(msg)
+      except requests.exceptions.Timeout:
+        msg = url + ': ' + "ConnectionError"
+        print(msg)
+      except Exception as why:
+        msg = url + ': ' + str(why)
+        print(msg)
+
+  else:
+    print("URL(s) to monitor have not been provided in config")

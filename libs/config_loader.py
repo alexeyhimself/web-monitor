4b67c238812460a7018736a7bb55bb825498107c
Alexey Sotskov <ihimself@gmail.com>
2021-01-26 00:13:17 +0300

    added support of dev config to config_loader and added dev config file to .gitignore

--- libs/config_loader.py
+++ libs/config_loader.py
@@ -1,12 +1,21 @@
 import sys
 import json
+import os.path
 
 CONFIG_PATH = 'config.json'
+CONFIG_PATH_DEV = 'config.dev.json'
 
 
 def load_config():
+  # For dev and test purposes CONFIG_PATH_DEV file has higher priority over 
+  # CONFIG_PATH. CONFIG_PATH_DEV file also is in .gitignore
+  if os.path.isfile(CONFIG_PATH_DEV):
+    cfg_path = CONFIG_PATH_DEV
+  else:
+    cfg_path = CONFIG_PATH
+
   try:
-    with open(CONFIG_PATH) as file:
+    with open(cfg_path) as file:
       cfg_file = file.read()
     cfg = json.loads(cfg_file)
     return cfg

055259ed096e699e56a23e149c1855f75182cbf7
Alexey Sotskov <ihimself@gmail.com>
2021-01-26 14:41:38 +0300

    reduced code and unnesessary periodic calls of functions

--- libs/urls_caller.py
+++ libs/urls_caller.py
@@ -7,10 +7,7 @@ DEFAULT_TIMEOUT = 10  # time to wait request response [seconds]
 DEFAULT_PERIOD = 60  # time between two consecutive requests [seconds]
 
 
-def call_url(each_url):
-  url = each_url.get("url")
-  timeout = each_url.get("timeout", DEFAULT_TIMEOUT)
-
+def call_url(url, timeout):
   try:
     r = requests.get(url, timeout=timeout)
     msg = url + ': ' + str(r.status_code)
@@ -27,28 +24,22 @@ def call_url(each_url):
     print(msg)
 
 
-def wait_till_next_call(each_url):
-  period = each_url.get("period", DEFAULT_PERIOD)
-  time.sleep(period)
-
-
-def async_call(proc):
-  proc.start()
-  proc.join()
-
-
 def monitor_url(each_url):
-  validate_period_and_timeout(each_url)
-
-  while True:
-    async_call(Process(target=call_url, args=(each_url,)))
-    async_call(Process(target=wait_till_next_call, args=(each_url,)))
-
-
-def validate_period_and_timeout(each_url):
   url = each_url.get("url")
   period = each_url.get("period", DEFAULT_PERIOD)
   timeout = each_url.get("timeout", DEFAULT_TIMEOUT)
+
+  validate_period_and_timeout(period, timeout)
+
+  while True:
+    proc = Process(target=call_url, args=(url, timeout,))
+    proc.start()
+    proc.join()
+    
+    time.sleep(period)
+
+
+def validate_period_and_timeout(period, timeout):
   if timeout > period:
     msg = "Timeout can't be greater than period. "
     msg += "But for %s it is: %s > %s" % (url, timeout, period)

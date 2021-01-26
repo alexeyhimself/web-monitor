ebbcc49c2c731170ccfab8fd8b8fffed56b5e944
Alexey Sotskov <ihimself@gmail.com>
2021-01-26 22:29:33 +0300

    prettified cfg_path declaration in load_config

--- libs/config_loader.py
+++ libs/config_loader.py
@@ -5,18 +5,14 @@ import os.path
 import logging
 logger = logging.getLogger(__name__)
 
+# For dev and test purposes CONFIG_PATH_DEV file has higher priority over 
+# CONFIG_PATH. CONFIG_PATH_DEV file also is in .gitignore
 CONFIG_PATH = 'configs/config.json'
 CONFIG_PATH_DEV = 'configs/config.dev.json'
+cfg_path = CONFIG_PATH_DEV if os.path.isfile(CONFIG_PATH_DEV) else CONFIG_PATH
 
 
 def load_config():
-  # For dev and test purposes CONFIG_PATH_DEV file has higher priority over 
-  # CONFIG_PATH. CONFIG_PATH_DEV file also is in .gitignore
-  if os.path.isfile(CONFIG_PATH_DEV):
-    cfg_path = CONFIG_PATH_DEV
-  else:
-    cfg_path = CONFIG_PATH
-
   try:
     with open(cfg_path) as file:
       cfg_file = file.read()

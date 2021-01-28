49eef368023cb25fd67d48895a24be7213b4c812
Alexey Sotskov <ihimself@gmail.com>
2021-01-28 10:38:58 +0300

    added comments to the logger code

--- libs/logger_loader.py
+++ libs/logger_loader.py
@@ -1,8 +1,15 @@
-import logging  # do not log requests, urllib3 in DEBUG/INFO level
+# do not log requests, urllib3, kafka in DEBUG/INFO level
+import logging
 logging.getLogger("requests").setLevel(logging.WARNING)
 logging.getLogger("urllib3").setLevel(logging.WARNING)
 logging.getLogger("kafka").setLevel(logging.WARNING)
 
+
+# .local_debug file that is in .gitignore that helps to distinguish:
+# DEV or PRO machine and provides options for fast switching of configuration.
+# For example, changing log_level (to see what PRO logs will look like to make) 
+# them more comprehensive or add more log details when developing features 
+# without changing this file. 
 import os.path
 from libs.config_loader import load_config
 ld_path = '.local_debug'
@@ -16,6 +23,8 @@ else:
 handler = ld_cfg.get("log_handler", "console") if DEBUG == True else "file"
 level = ld_cfg.get("log_level", "DEBUG") if DEBUG == True else "INFO"
 
+
+# Logs output customization and applying
 from logging.config import dictConfig
 
 log_format = "%(asctime)s.%(msecs)03d | %(levelname)s | %(name)s.py | "
